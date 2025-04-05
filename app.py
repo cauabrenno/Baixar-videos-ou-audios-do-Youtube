41f18fa621ced58740ce1fd043a2573f5f139415
cauabrenno <cauabrenno007@gmail.com>
2025-04-05 00:15:47 -0300

    Alterações no donwload

--- app.py
+++ app.py
@@ -1,48 +1,51 @@
 import os
+import re
 from pytubefix import YouTube
-from pytubefix.cli import on_progress
 from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
-# Pasta onde os arquivos serão salvos
+# Pasta de download
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
-# Função para atualizar o progresso do download
+# Limpa os caracteres inválidos do nome do arquivo
+def limpar_nome(nome):
+    """Remove caracteres inválidos do nome do arquivo"""
+    return re.sub(r'[\\/*?:"<>|]', "", nome).strip()
+
+# Barra de progresso no terminal
 def on_progress(stream, chunk, bytes_remaining):
-    global download_progress
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
-    download_progress = int((bytes_downloaded / total_size) * 100)
+    progresso = int((bytes_downloaded / total_size) * 100)
+print(f"\rProgresso: {progresso}%", end="")
 
+
+# interface web do Index.html
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Rota para processar o download
 @app.route("/download", methods=["POST"])
 def download():
-    global download_progress
-    download_progress = 0  # Resetar progresso
-
     try:
         url = request.form["url"]
-        download_type = request.form["download_type"]  # "video" ou "audio"
-        
+        download_type = request.form["download_type"]
+
         yt = YouTube(url, on_progress_callback=on_progress)
-        print("Baixando", yt.title)
+        print(f"\nBaixando: {yt.title}")
 
-        if download_type == "video":
-            stream = yt.streams.get_highest_resolution()
-            filename = f"{yt.title}.mp4"
-        else:
-            stream = yt.streams.filter(only_audio=True).first()
-            filename = f"{yt.title}.mp3"
-
+        titulo_limpo = limpar_nome(yt.title)
+        filename = f"{titulo_limpo}.mp4" if download_type == "video" else f"{titulo_limpo}.mp3"
         file_path = os.path.join(DOWNLOAD_FOLDER, filename)
+
+        # Baixar o arquivo na pasta downloads
+        stream = yt.streams.get_highest_resolution() if download_type == "video" else yt.streams.filter(only_audio=True).first()
         stream.download(output_path=DOWNLOAD_FOLDER, filename=filename)
 
-        print("Download Concluído")
+        print("\nDownload concluído.")
         return send_file(file_path, as_attachment=True)
 
     except Exception as e:
